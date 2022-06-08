bbafe4f79e79c9dae9fb61ac01fd56269e7501bc
Md Asif Alam Alam <v-mdaalam@microsoft.com>
2022-06-08 16:26:23 +0530

    commit 2

--- node-express-course-main/03-task-manager/starter/app.js
+++ node-express-course-main/03-task-manager/starter/app.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
 
+//middle ware
+app.use(express.json())
+
+//routes
 app.get('/hello', (req, res) => {
   res.send('Task Manager App')
 })
+
+app.use('/api/v1/tasks', tasks)
+
+
+// app.get('/api/v1/tasks') - get all the tasks
+// app.post('/api/v1/tasks') - Create a new tasks
+// app.get('/api/v1/tasks/:id') - get a single tasks
+// app.patch('/api/v1/tasks/:id') - Update Task
+// app.delete('/api/v1/tasks/:id') - delete task
+
+
+
+
 
 const port = 5500
 
